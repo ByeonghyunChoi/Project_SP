493b801b60c0d8866dc21840293806edda8437c9
ByeongHyunChoi <cqg0112@gmail.com>
2025-06-25 14:43:25 +0900

    프로젝트 업로드

---
+++ Source/Project_SP/Project_SP.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_SP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_SP, "Project_SP" );
+
+DEFINE_LOG_CATEGORY(LogProject_SP)
+ 
