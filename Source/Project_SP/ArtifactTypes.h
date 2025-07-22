8f92b395d62852ecd7baa871cf139d8e62682112
MArine123456 <86579994+MArine123456@users.noreply.github.com>
2025-07-22 16:59:14 +0900

    기존 아티팩트시스템 삭제 및 새로 제작
    기존 아티팩트시스템 삭제 및 새로 제작

---
+++ Source/Project_SP/ArtifactTypes.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ArtifactTypes.generated.h" 
+
+UENUM(BlueprintType)
+enum class EArtifactType : uint8
+{
+	HPUp,
+	ATKUp,
+	DEFUp,
+	SPDUp,
+	Special
+};
+
+UENUM(BlueprintType)
+enum class ERarity : uint8
+{
+	Normal,
+	Rare,
+	Unique,
+	Legendary,
+	Mystic
+};
+
+UENUM(BlueprintType)
+enum class ESetType : uint8
+{
+	Crystal,
+	Emerald,
+	Gold
+};
+
+UENUM(BlueprintType)
+enum class ESpecialStatType : uint8
+{
+	CriticalChance,
+	CriticalDamage,
+	ArmorPenetration,
+	AttackPercent,
+	DefensePercent,
+	HpPercent,
+	SpeedPercent
+};
+
+USTRUCT(BlueprintType)
+struct FStatBonus
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float HP;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Attack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Defense;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TMap<ESpecialStatType, float> SpecialStats;
+
+	FStatBonus()
+		: HP(0), Attack(0), Defense(0), Speed(0)
+	{
+	}
+};
