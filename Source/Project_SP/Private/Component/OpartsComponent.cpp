d37abe414ec6ee899ea2faa8ec9f9dc1a1ac7626
MArine123456 <86579994+MArine123456@users.noreply.github.com>
2025-10-16 21:45:18 +0900

    일단 오파츠 베이스만 추가

---
+++ Source/Project_SP/Private/Component/OpartsComponent.cpp
@@ -0,0 +1,10 @@
+
+#include "Component/OpartsComponent.h"
+
+// Sets default values for this component's properties
+UOpartsComponent::UOpartsComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
