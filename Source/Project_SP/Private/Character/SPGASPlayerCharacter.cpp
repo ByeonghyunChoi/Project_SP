a6a4b390247579cb33994d856fdcc698167d673d
Byeonghyun Choi <153609703+ByeonghyunChoi@users.noreply.github.com>
2026-05-20 11:58:58 +0900

    디버그 제거

--- Source/Project_SP/Private/Character/SPGASPlayerCharacter.cpp
+++ Source/Project_SP/Private/Character/SPGASPlayerCharacter.cpp
@@ -140,8 +140,8 @@ void ASPGASPlayerCharacter::PossessedBy(AController* NewController)
 	SetCameraProfile(FieldCameraSetting);
 	ReportReadyToGameMode();
 
-	APlayerController* PlayerController = CastChecked<ASPGASPlayerController>(NewController);
-	PlayerController->ConsoleCommand(TEXT("showdebug abilitysystem"));
+	/*APlayerController* PlayerController = CastChecked<ASPGASPlayerController>(NewController);
+	PlayerController->ConsoleCommand(TEXT("showdebug abilitysystem"));*/
 }
 
 
